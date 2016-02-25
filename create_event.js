8df80bae8f1e0ac0c5bd14ad51ac8b8071110f22
Anne Xie <anne_xie88@hotmail.com>
2016-02-25 09:30:58 -0600

    Update create_event.js

--- create_event.js
+++ create_event.js
@@ -75,3 +75,40 @@ $.mobile.document
         listview.before( form );
     });
 })( jQuery );
+
+
+$(function() {
+    $("#uploadFile").on("change", function()
+    {
+        var files = !!this.files ? this.files : [];
+        if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support
+        
+        if (/^image/.test( files[0].type)){ // only image file
+            var reader = new FileReader(); // instance of the FileReader
+            reader.readAsDataURL(files[0]); // read the local file
+            
+            reader.onloadend = function(){ // set image data as background of div
+                $("#imagePreview").css("background-image", "url("+this.result+")");
+            };
+        }
+    });
+});
+
+$(function() {
+    $("#uploadFile2").on("change", function()
+    {
+        var files = !!this.files ? this.files : [];
+        if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support
+        
+        if (/^image/.test( files[0].type)){ // only image file
+            var reader = new FileReader(); // instance of the FileReader
+            reader.readAsDataURL(files[0]); // read the local file
+            
+            reader.onloadend = function(){ // set image data as background of div
+                $("#imagePreview2").css("background-image", "url("+this.result+")");
+            };
+        }
+    });
+});
+
+
